2ad2a587bf663de1f50ed07b8e4a7e0018ae1e51
woodb58 <woodb58@gmail.com>
2022-05-28 10:56:35 -0500

    base server and adding on to file set up

---
+++ seeds/index.js
@@ -0,0 +1,12 @@
+const sequelize = require('../config/connection');
+
+// const seedAll = async () => {
+//     await sequelize.sync({ force: true });
+  
+//     await seed"name"();
+  
+  
+//     process.exit(0);
+//   };
+  
+//   seedAll();
