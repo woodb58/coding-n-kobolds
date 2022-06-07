0dd10ec6032ed8ecb6c55851f3f523f7082b9e40
woodb58 <woodb58@gmail.com>
2022-06-07 15:19:02 -0500

    login functionality

--- public/js/login.js
+++ public/js/login.js
@@ -1,19 +1,21 @@
 async function loginFormHandler(event) {
   event.preventDefault();
 
+  const username = document.querySelector('#username-login').value.trim();
   const password = document.querySelector('#password-login').value.trim();
 
-  if (password) {
-    const response = await fetch('/api/homepage', {
+  if (username && password) {
+    const response = await fetch('/api/users/login', {
       method: 'post',
       body: JSON.stringify({
+        username,
         password
       }),
       headers: { 'Content-Type': 'application/json' }
     });
 
     if (response.ok) {
-      document.location.replace('/user');
+      document.location.replace('/dashboard/');
     } else {
       alert(response.statusText);
     }
