81d78ddc0cfd8e59f42ed1003ad359a996412d20
woodb58 <woodb58@gmail.com>
2022-06-07 15:24:45 -0500

    path fix on login page

--- public/js/login.js
+++ public/js/login.js
@@ -5,7 +5,7 @@ async function loginFormHandler(event) {
   const password = document.querySelector('#password-login').value.trim();
 
   if (username && password) {
-    const response = await fetch('/api/users/login', {
+    const response = await fetch('/api/user', {
       method: 'post',
       body: JSON.stringify({
         username,
@@ -15,7 +15,7 @@ async function loginFormHandler(event) {
     });
 
     if (response.ok) {
-      document.location.replace('/dashboard/');
+      document.location.replace('/user');
     } else {
       alert(response.statusText);
     }
