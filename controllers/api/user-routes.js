57d36d871453c03bb7a4f9f8aeffa64d4c4dfb3d
bennettem <emily.pamelabennett@gmail.com>
2022-06-08 10:18:49 -0500

    added passport to package

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -1,38 +1,38 @@
-const router = require('express').Router();
-const { User, Toon } = require('../../models');
+const router = require("express").Router();
+const { User, Toon } = require("../../models");
 
-router.get('/', (req, res) => {
+// app.use(express.urlencoded({ extended: false }));
+
+router.get("/", (req, res) => {
   User.findAll()
-    .then(dbUserData => res.json(dbUserData))
-    .catch(err => {
+    .then((dbUserData) => res.json(dbUserData))
+    .catch((err) => {
       console.log(err);
       res.status(500).json(err);
-    })
-})
+    });
+});
 
-router.get('/:id', (req, res) => {
+router.get("/:id", (req, res) => {
   User.findOne({
     where: {
-      id: req.params.id
+      id: req.params.id,
     },
-    include: [
-      { model: Toon }
-    ]
+    include: [{ model: Toon }],
   })
-    .then(dbUserData => {
+    .then((dbUserData) => {
       if (!dbUserData) {
-        res.status(404).json({ message: 'ideth unfoundith' });
+        res.status(404).json({ message: "ideth unfoundith" });
         return;
       }
       res.json(dbUserData);
     })
-    .catch(err => {
+    .catch((err) => {
       console.log(err);
-      res.status(500).json(err)
+      res.status(500).json(err);
     });
 });
 
-router.post('/', async (req, res) => {
+router.post("/", async (req, res) => {
   try {
     const newUser = await User.create({
       username: req.body.username,
@@ -49,12 +49,12 @@ router.post('/', async (req, res) => {
   }
 });
 
-router.post('/', (req, res) => {
+router.post("/", (req, res) => {
   User.create({
     username: req.body.username,
-    password: req.body.password
+    password: req.body.password,
   })
-    .then(dbUserData => {
+    .then((dbUserData) => {
       req.session.save(() => {
         req.session.user_id = dbUserData.id;
         req.session.username = dbUserData.username;
@@ -63,27 +63,31 @@ router.post('/', (req, res) => {
         res.json(dbUserData);
       });
     })
-    .catch(err => {
+    .catch((err) => {
       console.log(err);
       res.status(500).json(err);
     });
 });
 
-router.post('/login', (req, res) => {
+router.post("/login", (req, res) => {
   User.findOne({
     where: {
-      username: req.body.username
-    }
-  }).then(dbUserData => {
+      username: req.body.username,
+    },
+  }).then((dbUserData) => {
     if (!dbUserData) {
-      res.status(400).json({ message: 'the realm has no record of this email!' });
+      res
+        .status(400)
+        .json({ message: "the realm has no record of this email!" });
       return;
     }
 
     const validPassword = dbUserData.checkPassword(req.body.password);
 
     if (!validPassword) {
-      res.status(400).json({ message: 'you shall not pass with incorrect passwords!' });
+      res
+        .status(400)
+        .json({ message: "you shall not pass with incorrect passwords!" });
       return;
     }
 
@@ -92,12 +96,12 @@ router.post('/login', (req, res) => {
       req.session.username = dbUserData.username;
       req.session.loggedIn = true;
 
-      res.json({ user: dbUserData, message: 'your in has been logged!' });
+      res.json({ user: dbUserData, message: "your in has been logged!" });
     });
   });
 });
 
-router.post('/logout', (req, res) => {
+router.post("/logout", (req, res) => {
   if (req.session.loggedIn) {
     req.session.destroy(() => {
       res.status(204).end();
@@ -107,40 +111,42 @@ router.post('/logout', (req, res) => {
   }
 });
 
-router.put('/:id', (req, res) => {
+router.put("/:id", (req, res) => {
   User.update(req.body, {
     individualHooks: true,
     where: {
-      id: req.params.id
-    }
+      id: req.params.id,
+    },
   })
-    .then(dbUserData => {
+    .then((dbUserData) => {
       if (!dbUserData) {
-        res.status(404).json({ message: 'no user in the realm found with this id' });
+        res
+          .status(404)
+          .json({ message: "no user in the realm found with this id" });
         return;
       }
       res.json(dbUserData);
     })
-    .catch(err => {
+    .catch((err) => {
       console.log(err);
       res.status(500).json(err);
     });
 });
 
-router.delete('/:id', (req, res) => {
+router.delete("/:id", (req, res) => {
   User.destroy({
     where: {
-      id: req.params.id
-    }
+      id: req.params.id,
+    },
   })
-    .then(dbUserData => {
+    .then((dbUserData) => {
       if (!dbUserData) {
-        res.status(404).json({ message: 'thou id hath not been found' });
+        res.status(404).json({ message: "thou id hath not been found" });
         return;
       }
       res.json(dbUserData);
     })
-    .catch(err => {
+    .catch((err) => {
       console.log(err);
       res.status(500).json(err);
     });
--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -1,26 +1,26 @@
 const router = require("express").Router();
-const sequelize = require('../config/connection');
+const sequelize = require("../config/connection");
 const withAuth = require("../utils/auth");
-const { Toon, User } = require('../models');
+const { Toon, User } = require("../models");
 
-router.get('/login', (req, res) => {
-    res.render('login');
+router.get("/login", (req, res) => {
+  res.render("login");
 });
 
-router.get('/', (req, res) => {
-    res.render('homepage')
+router.get("/", (req, res) => {
+  res.render("homepage");
 });
 
-router.get('/user', (req, res) => {
-    res.render('user')
+router.get("/user", (req, res) => {
+  res.render("user");
 });
 
-router.get('/create-toon', (req, res) => {
-    res.render('create-toon')
+router.get("/create-toon", (req, res) => {
+  res.render("create-toon");
 });
 
-router.get('/signup', (req, res) => {
-    res.render('signup')
+router.get("/signup", (req, res) => {
+  res.render("signup");
 });
 
 module.exports = router;
