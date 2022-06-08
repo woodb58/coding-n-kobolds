d471d6f8c9dca9f63c6bf2505b889872a9650358
Cameron <sicklecj@gmail.com>
2022-06-07 22:45:02 -0500

    pulled from develop

--- public/js/addToon.js
+++ public/js/addToon.js
@@ -1,13 +1,13 @@
 async function newFormHandler(event) {
     event.preventDefault();
 
+
+
     const toonName = document.querySelector('input[name="toonName"]').value;
-    const race = document.querySelector('input[name="race"]').value;
-    const gender = document.querySelector('input[name="gender"]').value;
-    const toonClass = document.querySelector('input[name="toonClass"]').value;
-    const backstory = document.querySelector('input[name="backstory"]').value;
-
-
+    const race = document.querySelector('select[name="race"]').option.value;
+    const gender = document.querySelector('select[name="gender"]').option.value;
+    const toonClass = document.querySelector('select[name="toonClass"]').option.value;
+    const backstory = document.querySelector('textarea[name="backstory"]').innerHTML;
 
     const response = await fetch(`/api/toon`, {
         method: 'POST',
@@ -24,6 +24,11 @@ async function newFormHandler(event) {
     });
 
     if (response.ok) {
+        console.log("============================================================================" + toonName);
+        console.log(race);
+        console.log(gender);
+        console.log(toonClass);
+        console.log(backstory);
         document.location.replace('/user');
     } else {
         alert(response.statusText);
