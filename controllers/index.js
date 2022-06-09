cfd4ff9c954b0b7b28cf833279e84f26dfd92a2c
woodb58 <woodb58@gmail.com>
2022-06-09 18:39:59 -0500

    removed un-used route

--- controllers/index.js
+++ controllers/index.js
@@ -2,10 +2,8 @@ const router = require('express').Router();
 
 const apiRoutes = require('./api');
 const homeRoutes = require('./home-routes.js');
-const userRoutes = require('./user-routes.js');
 
 router.use('/api', apiRoutes);
 router.use('/', homeRoutes);
-router.use('/', userRoutes);
 
 module.exports = router;
